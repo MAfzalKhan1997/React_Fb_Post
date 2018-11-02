4e43ee6f183d5fda02f89bbb0138cf6dea8de52b
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2018-11-02 05:48:52 +0500

    fb-post

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import '../../App.css';
 
-import 'typeface-roboto';  
+import 'typeface-roboto';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles'; 
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader'; 
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions'; 
+import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
@@ -29,24 +29,25 @@ const styles = theme => ({
   },
   actions: {
     display: 'flex',
-  }, 
+  },
   avatar: {
     backgroundColor: red[500],
   },
 });
 
-class RecipeReviewCard extends React.Component { 
+class RecipeReviewCard extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
   }
-  
+
   render() {
-    const { classes,images } = this.props;
+    const { classes, images } = this.props;
 
     return (
       <Card className={classes.card}>
         <CardHeader
+          className='nomargin'
           avatar={
             <Avatar aria-label="Recipe" className={classes.avatar}>
               R
@@ -57,37 +58,38 @@ class RecipeReviewCard extends React.Component {
               <MoreVertIcon />
             </IconButton>
           }
-  title={<Typography variant="headline" color="textPrimary" style={{}}>Shrimp and Chorizo Paella</Typography>}
-          // title=""
-          // subheader="September 14, 2016"
+          title={<Typography color="primary" align='left' className='fbname'>Muhammad Afzal Khan</Typography>}
+          subheader={<Typography variant='caption' align='left' className='fbdate'>September 14, 2016</Typography>}
         />
         {/* <CardMedia
           className={classes.media}
           image='https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg'
           title="Contemplative Reptile"
           /> */}
-          <CardContent>
-            <Typography variant="a" align='left'>
-              This impressive paella is a perfect party dish and a fun meal to cook together with your
-              guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        <CardContent className='nomargin'>
+          
+          <Typography variant="a" align='left'>
+            This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like.
             </Typography>
-          </CardContent>
-          { <FbImageLibrary
-              images={images}
-            // countFrom={2}
-            // hideOverlay={true}
-            // onClickEach={({src, index}) => {console.log(src,index)}}
-            // renderOverlay={() => <button>Show Image</button>}
-            // overlayBackgroundColor={'green' or '#000000' or 'rgb(255, 26, 26)'}
-            /> }
+        </CardContent>
+        {<FbImageLibrary
+        
+          images={images}
+        // countFrom={2}
+        // hideOverlay={true}
+        // onClickEach={({src, index}) => {console.log(src,index)}}
+        // renderOverlay={() => <button>Show Image</button>}
+        // overlayBackgroundColor={'green' or '#000000' or 'rgb(255, 26, 26)'}
+        />}
         <CardActions className={classes.actions} disableActionSpacing>
           <IconButton aria-label="Add to favorites">
             <FavoriteIcon />
           </IconButton>
           <IconButton aria-label="Share">
             <ShareIcon />
-          </IconButton> 
-        </CardActions> 
+          </IconButton>
+        </CardActions>
       </Card>
     );
   }
